f01123632794650146adf5e15c7ca7a77c06fa04
sohan <sohan.vijayakumar@mindtree.com>
2019-05-25 13:45:42 +0530

    Initial commit

---
+++ app/src/main/java/com/najdi/android/najdiapp/MainActivity.java
@@ -0,0 +1,13 @@
+package com.najdi.android.najdiapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
