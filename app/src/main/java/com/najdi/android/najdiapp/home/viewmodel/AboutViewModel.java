438d6ac64037ff971223df9ece40e725df922cfe
sohan90 <sohan.shetty111@gmail.com>
2020-07-26 12:23:05 +0530

    Refactored class and methods
    Signed-off-by: sohan90 <sohan.shetty111@gmail.com>

--- app/src/main/java/com/najdi/android/najdiapp/home/viewmodel/AboutViewModel.java
+++ app/src/main/java/com/najdi/android/najdiapp/home/viewmodel/AboutViewModel.java
@@ -4,21 +4,11 @@ import android.app.Application;
 
 import androidx.annotation.NonNull;
 import androidx.lifecycle.LiveData;
-import androidx.lifecycle.MutableLiveData;
 
 import com.najdi.android.najdiapp.common.BaseResponse;
 import com.najdi.android.najdiapp.common.BaseViewModel;
 
 public class AboutViewModel extends BaseViewModel {
-    private static final int ABOUT_US_ENG = 576;
-    private static final int ABOUT_US_ARABIC = 1092;
-    private static final int TERMS_ENG = 238;
-    private static final int TERMS_ARABIC = 2533;
-    private static final int PRIVACY_ENG = 2531;
-    private static final int PRIVACY_ARABIC = 2536;
-
-    private MutableLiveData<String> content = new MutableLiveData<>();
-
     public AboutViewModel(@NonNull Application application) {
         super(application);
     }
@@ -36,11 +26,4 @@ public class AboutViewModel extends BaseViewModel {
         return repository.getTermsCondition();
     }
 
-    public void setHtmlContent(String content){
-        this.content.setValue(content);
-    }
-
-    public MutableLiveData<String> getContent() {
-        return content;
-    }
 }
